4ff4e8c5265d9e45b00dd5e27f1486d070360b05
Ethan Zhang <ethanzhang45@gmail.com>
2024-06-06 18:40:15 -0400

    test implementation

--- test/StockPriceTest.java
+++ test/StockPriceTest.java
@@ -1,5 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import model.StockPrice;
+
 import static org.junit.Assert.*;
 
 public class StockPriceTest {
+  private double openingPrice;
+  private double closingPrice;
+  private double lowPrice;
+  private double highPrice;
+  private LocalDate date;
+  private StockPrice stockPrice;
+  @Before
+  public void setUp() {
+    openingPrice = 100.0;
+    closingPrice = 110.0;
+    lowPrice = 90.0;
+    highPrice = 120.0;
+    date = LocalDate.of(2024, 6, 5);
+    stockPrice = new StockPrice(openingPrice, closingPrice, lowPrice, highPrice, date);
+  }
 
+  @Test
+  public void testGetOpeningPrice() {
+    assertEquals(openingPrice, stockPrice.getOpeningPrice(), 0.01);
+  }
+  @Test
+  public void testGetClosingPrice() {
+    assertEquals(closingPrice, stockPrice.getClosingPrice(), 0.01);
+  }
+  @Test
+  public void testGetLowPrice() {
+    assertEquals(lowPrice, stockPrice.getLowPrice(), 0.01);
+  }
+  @Test
+  public void testGetHighPrice() {
+    assertEquals(highPrice, stockPrice.getHighPrice(), 0.01);
+  }
+  @Test
+  public void testGetDate() {
+    assertEquals(date, stockPrice.getDate());
+  }
 }
